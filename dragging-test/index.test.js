db331c732246178a6ba5ed6fcffbc9514345af27
itai brand <67857476+BrandMan2299@users.noreply.github.com>
2020-10-04 11:50:32 +0300

    bottom right border test

--- dragging-test/index.test.js
+++ dragging-test/index.test.js
@@ -1,11 +1,11 @@
 const puppeteer = require("puppeteer");
-describe("dragging box", () => {
+describe("dragging box", async () => {
   test("dragging", async () => {
     const browser = await puppeteer.launch(
-      {
-        headless: false,
-        slowMo: 100,
-      }
+      // {
+      //   headless: false,
+      //   slowMo: 100,
+      // }
     );
     const url =
       "file:///C:/Users/itaib/Desktop/cyber4s/Mine/drag-and-scale/dragging-test/index.html";
@@ -26,7 +26,6 @@ describe("dragging box", () => {
     await page.mouse.move(2 * left, 2 * top);
     await page.mouse.up();
 
-
     const { newTop, newLeft } = await page.evaluate((header) => {
       const { top, left } = header.getBoundingClientRect();
       return { newTop: top + header.clientHeight / 2, newLeft: left + header.clientWidth / 2 };
@@ -37,7 +36,7 @@ describe("dragging box", () => {
 
     await browser.close();
   });
-  test("can't go past the borders", () => {
+  test("can't go past the borders", async () => {
     const browser = await puppeteer.launch(
       // {
       //   headless: false,
@@ -50,18 +49,34 @@ describe("dragging box", () => {
 
     const vpWidth = page.viewport().width;
     const vpHeight = page.viewport().height;
+
+    const box = await page.$('#mydiv');
+    const { boxWidth, boxHeight } = await page.evaluate((box) => {
+      let { width, height } = box.getBoundingClientRect();
+      return { boxWidth: width, boxHeight: height };
+    }, box);
+
     const header = await page.$('#mydivheader');
 
-    const { top, left } = await page.evaluate((header) => {
-      let { top, left } = header.getBoundingClientRect();
-      top += header.clientHeight / 2;
-      left += header.clientWidth / 2;
-      return { top, left };
+    const { y, x } = await page.evaluate((header) => {
+      let { top, left, width, height } = header.getBoundingClientRect();
+      return { y: top + height / 2, x: left + width / 2, headerWidth: width, headerHeight: height };
     }, header);
 
-    await page.mouse.move(left, top);
+    await page.mouse.move(x, y);
     await page.mouse.down();
-    await page.mouse.move(2 * left, 2 * top);
+    await page.mouse.move(vpWidth, vpHeight);
     await page.mouse.up();
+
+    const { newTop, newLeft } = await page.evaluate((box) => {
+      const { top, left } = box.getBoundingClientRect();
+      return { newTop: top, newLeft: left };
+    }, box);
+
+    expect(newLeft).toBe(vpWidth - boxWidth);
+    expect(newTop).toBe(vpHeight - boxHeight);
+
+    await browser.close();
   })
+
 })
